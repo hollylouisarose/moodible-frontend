1facfc169f05cd992c66b66bcd70e682645b90f8
hollylouisarose <hlrstratton@gmail.com>
2021-09-15 20:46:44 +0100

    adjusted homepage

--- src/components/common/Home.js
+++ src/components/common/Home.js
@@ -14,6 +14,7 @@ function Home(){
               <h3>Inspiration without information overwhelm.</h3>
               <p>Mood.ible generates a curated moodboard of five inspiring images, based on your chosen ‘mood’.</p>
               <p>You will never see more than five at a time, this give you time to savour each one, without disappearing down a rabbit hole.</p>
+              <p>Your profile is the perfect place to store your favourite images and write your ideas down, ready for when you need them.</p>
             </div>
             <div className="column is-half">
               <figure>
@@ -21,15 +22,8 @@ function Home(){
               </figure>
             </div>
           </div>
-          <div className="columns">
-            <div className="column is-half">
-              <h3>Ideas strikes at weird times!</h3>
-              <p>Your profile is the perfect place to store your favourite images and write your ideas down, ready for when you need them.</p>
-            </div>
-          </div>
         </div>
-
-      </div>
+      </div> 
       <div className="hero">
         <h3 className="has-text-centered">More focus, less scrolling</h3>
         <button className="cta-button button">
