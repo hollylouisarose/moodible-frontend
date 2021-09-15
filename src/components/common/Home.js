25fdf531d7bebef60966d2feb65ba432e1ac7cff
hollylouisarose <hlrstratton@gmail.com>
2021-09-15 11:11:04 +0100

    initial content added

--- src/components/common/Home.js
+++ src/components/common/Home.js
@@ -1,10 +1,49 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import moodboardimage from '../../images/moodboard.png'
+
+
 function Home(){
 
   return (
-    <div className="content">
-      <h2>I am the homepage</h2>
-      <p>Paragraph test</p>
-    </div>
+    <section className="section">
+      <div className="content">
+        <div>
+          <div>
+            <h3>Inspiration without information overwhelm.</h3>
+            <p>Mood.ible generates a curated moodboard of five inspiring images, based on your chosen ‘mood’.</p>
+            <p>You will never see more than five at a time, this give you time to savour each one, without disappearing down a rabbit hole.</p>
+          </div>
+          <figure>
+            <img src={moodboardimage} />
+          </figure>
+
+          <button className="button">
+            <Link to="/signup">
+              Sign Up
+            </Link>
+          </button>
+        </div>
+        <div>
+          <h3>Ideas strikes at weird times!</h3>
+          <p>Your profile is the perfect place to store your favourite images and write your ideas down, ready for when you need them.</p>
+        </div>
+        <button className="button">
+          <Link to="/signup">
+            Sign Up
+          </Link>
+        </button>
+      </div>
+      <div className="hero">
+        <h2>More focus, less scrolling</h2>
+        <button className="button">
+          <Link to="/signup">
+            Sign Up
+          </Link>
+        </button>
+      </div>
+    </section>
+    
   ) 
 
 
