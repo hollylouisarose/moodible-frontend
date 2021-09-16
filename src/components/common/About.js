29905a98f96d561ae3f3ae808f29a89f02808a2f
hollylouisarose <hlrstratton@gmail.com>
2021-09-16 12:54:23 +0100

    added about page

--- src/components/common/About.js
+++ src/components/common/About.js
@@ -1,6 +1,29 @@
+import { Link } from 'react-router-dom'
+
 function About(){
 
-  return <h2>About Holly and Moodible</h2>
+  return (
+    <section className="section">
+      <div className="container">
+        <div className="columns">
+          <div className="column is-half-desktop is-two-thirds-tablet">
+            <h3>Thanks for visiting mood.ible!</h3>
+            <p>I made this site for my final project for my Software Engineering course at General Assembly</p>
+            <p>At time of writing, I am about to graduate and will be looking for opportunities so if you would like to chat, get in touch:</p>
+            <ul>
+              <li>
+                <Link to="https://www.linkedin.com/in/hstratton/">Linkedin</Link>
+              </li>
+              <li>
+                <Link to="https://github.com/hollylouisarose">GitHub</Link>
+              </li>
+              <p>Thanks! Holly 🙂</p>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
 
 
 }
